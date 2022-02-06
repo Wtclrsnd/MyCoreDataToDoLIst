b07ebbf1e14744c5c5f8c586da775e5ab8da27c5
Emil Shpeklord <emil42661@gmail.com>
2022-02-06 18:49:46 +0300

    Initial Commit

---
+++ CoreDataToDoLIst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataToDoLIst
+//
+//  Created by Emil Shpeklord on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
