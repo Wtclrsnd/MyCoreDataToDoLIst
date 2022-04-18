b9b54dde518d98f990b1bdd700980d780aacd788
Emil Shpeklord <emil42661@gmail.com>
2022-04-18 21:13:59 +0300

    added keyboard mapping

--- CoreDataToDoLIst/MainScene/DetailNoteViewController.swift
+++ CoreDataToDoLIst/MainScene/DetailNoteViewController.swift
@@ -37,14 +37,63 @@ class DetailNoteViewController: UIViewController {
 		let textView = UITextView()
 		textView.font = .systemFont(ofSize: 30)
 		textView.backgroundColor = .systemBackground
-		textView.contentInset = UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10)
+        textView.contentInset = UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10)
 		return textView
 	}()
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+        addObservers()
 		setUpUI()
 	}
+
+    deinit {
+        NotificationCenter.default.removeObserver(UIResponder.keyboardWillHideNotification)
+        NotificationCenter.default.removeObserver(UIResponder.keyboardWillShowNotification)
+        NotificationCenter.default.removeObserver(UIResponder.keyboardWillChangeFrameNotification)
+        print("Observers were removed")
+    }
+
+    private func addObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillChangeFrameNotification,
+            object: nil
+        )
+    }
+
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewAndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = .zero
+            navigationItem.rightBarButtonItem = nil
+        } else {
+            textView.contentInset = UIEdgeInsets(
+                top: 0,
+                left: 0,
+                bottom: keyboardViewAndFrame.height - view.safeAreaInsets.bottom,
+                right: 0
+            )
+        }
+
+        textView.scrollIndicatorInsets = textView.contentInset
+        let selectedRange = textView.selectedRange
+        textView.scrollRangeToVisible(selectedRange)
+    }
 
 	private func setUpUI() {
 		view.addSubview(stack)
